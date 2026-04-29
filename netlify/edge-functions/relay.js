1340011af783557be5059c7d3fd89dca914edab3
bludgamer <blud.gamer21@gmail.com>
2026-04-29 16:28:32 +0330

    newrelay.js

--- netlify/edge-functions/relay.js
+++ netlify/edge-functions/relay.js
@@ -1,4 +1,4 @@
-const TARGET_BASE = (Netlify.env.get("TARGET_DOMAIN") || "").replace(/\/$/, "");
+const TARGET_BASE = (Netlify.env.get("TARGET_DOMAIN=https://activemmd.duckdns.org:33163") || "").replace(/\/$/, "");
 
 const STRIP_HEADERS = new Set([
   "host",
